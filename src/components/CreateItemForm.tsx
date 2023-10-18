8f07c331d74a30a9482b51c112b2a85b178f3878
woodemai <n.savchenkoo73@gmail.com>
2023-10-18 23:13:13 +0300

    creating new item form almost done

--- src/components/CreateItemForm.tsx
+++ src/components/CreateItemForm.tsx
@@ -6,7 +6,32 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod"
+import axios from "axios"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 const formSchema = z.object({
     name: z.string().min(2, {
@@ -31,22 +56,197 @@ const formSchema = z.object({
     }).max(100, {
         message: "Storage condition must be less than 100 characters"
     }),
-    weight: z.number(),
-    price: z.number()
+    weight: z.coerce.number(),
+    price: z.coerce.number()
 })
 
 const CreateItemForm = () => {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            description: "",
+            manufacturer: "",
+            storageCondition: "",
+            productionDate: new Date(),
+            expirationDate: new Date(),
+            weight: 0,
+            price: 0
+        },
+    })
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+        axios.post("http://localhost/item", values)
+    }
+
     return (
         <Dialog>
             <DialogTrigger>Open</DialogTrigger>
             <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Create new item</DialogTitle>
-                    <DialogDescription>
-                        Create your new item
-                    </DialogDescription>
-
-                </DialogHeader>
+                <ScrollArea className="h-96">
+                    <DialogHeader>
+                        <DialogTitle>Create new item</DialogTitle>
+                        <DialogDescription>
+                            Create your new item
+                        </DialogDescription>
+                    </DialogHeader>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Moose Horns" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            New item's name
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="description"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Description</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="These are the strongest best and coolest horns" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            New item's description
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="manufacturer"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Manufacturer</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Horns and Hooves LLC" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            Manufacturer's company name
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="productionDate"
+                                render={({ field }) => (
+                                    <FormItem className="flex flex-col">
+                                        <FormLabel>Production date</FormLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <Button
+                                                    variant={"outline"}
+                                                    className={cn(
+                                                        "w-[280px] justify-start text-left font-normal",
+                                                        !field.value && "text-muted-foreground"
+                                                    )}
+                                                >
+                                                    <CalendarIcon className="mr-2 h-4 w-4" />
+                                                    {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+                                                </Button>
+                                            </PopoverTrigger>
+                                            <PopoverContent className="w-auto p-0">
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    initialFocus
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                        <FormDescription>
+                                            Item's production date
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="expirationDate"
+                                render={({ field }) => (
+                                    <FormItem className="flex flex-col">
+                                        <FormLabel>Expiration date</FormLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <Button
+                                                    variant={"outline"}
+                                                    className={cn(
+                                                        "w-[280px] justify-start text-left font-normal",
+                                                        !field.value && "text-muted-foreground"
+                                                    )}
+                                                >
+                                                    <CalendarIcon className="mr-2 h-4 w-4" />
+                                                    {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+                                                </Button>
+                                            </PopoverTrigger>
+                                            <PopoverContent className="w-auto p-0">
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    initialFocus
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                        <FormDescription>
+                                            Item's expiration date
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="weight"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Weight</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="65"  {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            New item's weight
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="price"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Price</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="73" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            New item's price
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit" >Create</Button>
+                        </form>
+                    </Form>
+                </ScrollArea>
             </DialogContent>
         </Dialog>
 
