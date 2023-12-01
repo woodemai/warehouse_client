4c040c7afb6c5c62d24888afb4d5ae6edad14971
woodemai <n.savchenkoo73@gmail.com>
2023-12-01 04:06:33 +0300

    fix bugs

--- src/entities/user/api/AuthService.ts
+++ src/entities/user/api/AuthService.ts
@@ -4,10 +4,10 @@ import axios, { AxiosResponse } from "axios";
 
 export class AuthService {
     static async login(email: string, password: string): Promise<AxiosResponse<AuthResponse >> {
-        return axios.post<AuthResponse>(`${API_URL}/v1/auth/login`, { email, password }, { withCredentials: true })
+        return axios.post<AuthResponse>(`${API_URL}v1/auth/login`, { email, password }, { withCredentials: true })
     }
     static async registration(email: string, password: string, role: string): Promise<AxiosResponse<AuthResponse>> {
-        return axios.post<AuthResponse>(`${API_URL}/v1/auth/registration`, { email, password, role }, { withCredentials: true })
+        return axios.post<AuthResponse>(`${API_URL}v1/auth/registration`, { email, password, role }, { withCredentials: true })
 
     }
     static async logout(): Promise<void> {
